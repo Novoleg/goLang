38495623fb096d5240c48220f1dfbbc22b27c4da
Oleg <novoleg2697@yandex.ru>
2020-11-26 11:50:06 +0700

    add log

--- cmd/goLang/main.go
+++ cmd/goLang/main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"context"
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	logrus.Info("Всем привет, я только проснулся")
+
+	log := logrus.New()
+	log.SetOutput(os.Stdout)
+
+	log.Info("Старт приложения")
 
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		logrus.Fatal("Порт не задан")
+		log.Fatal("Порт не задан")
 	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
-	http.ListenAndServe(":"+port, nil)
+
+	serv := http.Server{
+		Addr:    net.JoinHostPort("", port),
+		Handler: router,
+	}
+	go serv.ListenAndServe()
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	<-interrupt
+	log.Info("Остановка приложения")
+
+	timeout, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	err := serv.Shutdown(timeout)
+	if err != nil {
+		log.Error("Ошибка когда приложение упало: %v", err)
+	}
+
+	log.Info("Приложение остановлено")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Novoleg/goLang
 
 go 1.13
 
-require github.com/sirupsen/logrus v1.7.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.7.0
+)
